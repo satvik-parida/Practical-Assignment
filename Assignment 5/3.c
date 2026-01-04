680f8e90a45196fd94225f315a291fb06422f389
satvik-parida <satvik.parida@rungta.org>
2026-01-04 20:15:56 +0530

    Add files via upload

---
+++ Assignment 5/3.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+int main() {
+    char str[200];
+    int i, words = 1;
+    printf("Enter a sentence: ");
+    gets(str);
+    for (i = 0; str[i] != '\0'; i++) {
+        if (str[i] == ' ' && str[i + 1] != ' ')
+            words++;
+    }
+    printf("Number of words = %d\n", words);
+    return 0;
+}
